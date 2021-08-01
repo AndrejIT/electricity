d6404f9b470fd6bbcd75e71e3888deeb5dfa19fc
AndrejIT <andrejit@rambler.ru>
2021-08-01 19:30:14 +0300

    Piston with double length.

--- init.lua
+++ init.lua
@@ -870,7 +870,9 @@ function electricity.piston_on(pos, node)
         node0.name == "doors:door_steel_b_2" or
         node0.name == "doors:door_steel_t_2" or
         node0.name == "electricity:piston_on" or
-        node0.name == "electricity:piston_pusher_sticky"
+        node0.name == "electricity:piston2_on" or
+        node0.name == "electricity:piston_pusher_sticky" or
+        node0.name == "electricity:piston_pusher_part"
     then
         return false
     end
@@ -946,7 +948,9 @@ function electricity.piston_off(pos, node)
         node1.name == "doors:door_steel_b_2" or
         node1.name == "doors:door_steel_t_2" or
         node1.name == "electricity:piston_on" or
-        node1.name == "electricity:piston_pusher_sticky"
+        node1.name == "electricity:piston2_on" or
+        node1.name == "electricity:piston_pusher_sticky" or
+        node1.name == "electricity:piston_pusher_part"
     then
         minetest.swap_node(pos, node)
         if node0.name == "electricity:piston_pusher_sticky" then
@@ -1101,6 +1105,401 @@ minetest.register_node("electricity:piston_pusher_sticky", {
 	diggable = false,
 	selection_box = piston_pusher_box,
 	node_box = piston_pusher_box,
+	drop = "",
+	sounds = default.node_sound_wood_defaults(),
+})
+
+-- PISTONS X2
+-- Swap electricity node on or off
+function electricity.piston2_on_timer(self_pos, elapsed)
+    local node = minetest.get_node(self_pos)
+    local node_reg = minetest.registered_nodes[node.name]
+    if  node_reg and
+        node_reg.electricity
+    then
+        local volt = electricity.get(self_pos, self_pos)
+        if volt == 1 and node.name == node_reg.electricity.name_off then
+            node.name = node_reg.electricity.name_on
+            if node.name == "electricity:piston2_on" then
+                electricity.piston2_on(self_pos, node)
+            end
+        elseif volt == 0 and node.name == node_reg.electricity.name_on then
+            node.name = node_reg.electricity.name_off
+            if node.name == "electricity:piston2_off" then
+                electricity.piston2_off(self_pos, node)
+            end
+        end
+    end
+end
+
+function electricity.piston2_on(pos, node)
+    local face_vector = electricity.get_node_face_direction(pos)
+	local node0_pos = electricity.get_pos_relative(pos, {x=1,y=0,z=0}, face_vector, null)
+    local node1_pos = electricity.get_pos_relative(pos, {x=2,y=0,z=0}, face_vector, null)
+    local node2_pos = electricity.get_pos_relative(pos, {x=3,y=0,z=0}, face_vector, null)
+    local node3_pos = electricity.get_pos_relative(pos, {x=4,y=0,z=0}, face_vector, null)
+    local node0 = minetest.get_node(node0_pos)
+    local node1 = minetest.get_node(node1_pos)
+    local node2 = minetest.get_node(node2_pos)
+    local node3 = minetest.get_node(node3_pos)
+
+    -- Permission check?
+    local meta = minetest.get_meta(pos)
+    local owner = meta:get_string("owner")
+    if
+        minetest.is_protected(node0_pos, owner) or
+        minetest.is_protected(node1_pos, owner) or
+        minetest.is_protected(node2_pos, owner) or
+        minetest.is_protected(node3_pos, owner)
+    then
+        return false
+    end
+
+    -- Free space check
+    -- For now, move only both nodes at once. Or first one. Or second one. Or when all empty. ignore other cases
+    if
+        not(
+            not electricity.node_replaceable(node0.name) and
+            not electricity.node_replaceable(node1.name) and
+            electricity.node_replaceable(node2.name) and
+            electricity.node_replaceable(node3.name)
+        )
+        and not(
+            electricity.node_replaceable(node0.name) and
+            not electricity.node_replaceable(node1.name) and
+            electricity.node_replaceable(node2.name) and
+            electricity.node_replaceable(node3.name)
+        )
+        and not(
+            not electricity.node_replaceable(node0.name) and
+            electricity.node_replaceable(node1.name) and
+            electricity.node_replaceable(node2.name) and
+            electricity.node_replaceable(node3.name)
+        )
+        and not(
+            electricity.node_replaceable(node0.name) and
+            electricity.node_replaceable(node1.name) and
+            not electricity.node_replaceable(node2.name) and
+            not electricity.node_replaceable(node3.name)
+        )
+        and not(
+            electricity.node_replaceable(node0.name) and
+            electricity.node_replaceable(node1.name) and
+            electricity.node_replaceable(node2.name) and
+            electricity.node_replaceable(node3.name)
+        )
+    then
+        return false
+    end
+
+    -- Stoppers?
+    -- unknown nodes are always stoppers
+    if not minetest.registered_nodes[node0.name] and not minetest.registered_nodes[node1.name] then
+        return false
+    end
+
+    if
+        node0.name == "protector:protect" or
+        node0.name == "protector:protect2" or
+        node0.name == "protector_mese:protect" or
+        node0.name == "protector_mese:brazier_bronze" or
+        node0.name == "protector_mese:brazier_gold" or
+        node0.name == "doors:door_steel_b_1" or
+        node0.name == "doors:door_steel_t_1" or
+        node0.name == "doors:door_steel_b_2" or
+        node0.name == "doors:door_steel_t_2" or
+        node0.name == "electricity:piston_on" or
+        node0.name == "electricity:piston2_on" or
+        node0.name == "electricity:piston_pusher_sticky" or
+        node0.name == "electricity:piston_pusher_part"
+        or
+        node1.name == "protector:protect" or
+        node1.name == "protector:protect2" or
+        node1.name == "protector_mese:protect" or
+        node1.name == "protector_mese:brazier_bronze" or
+        node1.name == "protector_mese:brazier_gold" or
+        node1.name == "doors:door_steel_b_1" or
+        node1.name == "doors:door_steel_t_1" or
+        node1.name == "doors:door_steel_b_2" or
+        node1.name == "doors:door_steel_t_2" or
+        node1.name == "electricity:piston_on" or
+        node1.name == "electricity:piston2_on" or
+        node1.name == "electricity:piston_pusher_sticky" or
+        node1.name == "electricity:piston_pusher_part"
+    then
+        return false
+    end
+
+    -- Move objects if there is place for them
+    if electricity.node_replaceable(node0.name) and electricity.node_replaceable(node1.name) and electricity.node_replaceable(node2.name) then
+        local objects_to_move = electricity.get_move_objects(node0_pos)
+        -- combine with node0_pos?
+        electricity.move_objects(objects_to_move, node0_pos, node2_pos)
+    elseif electricity.node_replaceable(node2.name) then
+        local node4_pos = electricity.get_pos_relative(pos, {x=5,y=0,z=0}, face_vector, null)
+        local node4 = minetest.get_node(node4_pos)
+        if electricity.node_replaceable(node4.name) then
+            local objects_to_move = electricity.get_move_objects(node2_pos)
+            electricity.move_objects(objects_to_move, node2_pos, node4_pos)
+        end
+    end
+
+    -- Move nodes if there is space
+    if not electricity.node_replaceable(node0.name) and electricity.node_replaceable(node1.name) then
+    	local meta0 = minetest.get_meta(node0_pos):to_table()
+    	minetest.set_node(node2_pos, node0)
+    	minetest.get_meta(node2_pos):from_table(meta0)
+        local meta1 = minetest.get_meta(node1_pos):to_table()
+    	minetest.set_node(node3_pos, node1)
+    	minetest.get_meta(node3_pos):from_table(meta1)
+    elseif electricity.node_replaceable(node0.name) and not electricity.node_replaceable(node1.name) then
+    	local meta1 = minetest.get_meta(node1_pos):to_table()
+    	minetest.set_node(node2_pos, node1)
+    	minetest.get_meta(node2_pos):from_table(meta1)
+    elseif not electricity.node_replaceable(node0.name) and not electricity.node_replaceable(node1.name) then    -- do not replace nodes with air
+    	local meta0 = minetest.get_meta(node0_pos):to_table()
+    	minetest.set_node(node2_pos, node0)
+    	minetest.get_meta(node2_pos):from_table(meta0)
+        local meta1 = minetest.get_meta(node1_pos):to_table()
+    	minetest.set_node(node3_pos, node1)
+    	minetest.get_meta(node3_pos):from_table(meta1)
+    end
+    -- Add pusher
+	minetest.set_node(node0_pos, {name = "electricity:piston_pusher_part", param2 = node.param2})
+    minetest.set_node(node1_pos, {name = "electricity:piston_pusher_sticky", param2 = node.param2})
+
+    minetest.swap_node(pos, node)
+
+	minetest.sound_play("piston_extend", {
+		pos = pos,
+		max_hear_distance = 20,
+		gain = 0.3,
+	})
+
+    return true
+end
+
+function electricity.piston2_off(pos, node)
+    local face_vector = electricity.get_node_face_direction(pos)
+    local node0_pos = electricity.get_pos_relative(pos, {x=1,y=0,z=0}, face_vector, null)
+    local node1_pos = electricity.get_pos_relative(pos, {x=2,y=0,z=0}, face_vector, null)
+    local node2_pos = electricity.get_pos_relative(pos, {x=3,y=0,z=0}, face_vector, null)
+    local node3_pos = electricity.get_pos_relative(pos, {x=4,y=0,z=0}, face_vector, null)
+    local node0 = minetest.get_node(node0_pos)
+    local node1 = minetest.get_node(node1_pos)
+    local node2 = minetest.get_node(node2_pos)
+    local node3 = minetest.get_node(node3_pos)
+
+    -- Permission check?
+    local meta = minetest.get_meta(pos)
+    local owner = meta:get_string("owner")
+    if
+        minetest.is_protected(node0_pos, owner) or
+        minetest.is_protected(node1_pos, owner) or
+        minetest.is_protected(node2_pos, owner) or
+        minetest.is_protected(node3_pos, owner)
+    then
+        minetest.swap_node(pos, node)
+        if node0.name == "electricity:piston_pusher_part" then
+            minetest.set_node(node0_pos, { name = "air" })
+        end
+        if node1.name == "electricity:piston_pusher_sticky" then
+            minetest.set_node(node1_pos, { name = "air" })
+        end
+        return false
+    end
+
+    -- Stoppers?
+    -- unknown nodes are always stoppers
+    if not minetest.registered_nodes[node2.name] or not minetest.registered_nodes[node3.name] then
+        return true
+    end
+
+    if
+        node2.name == "protector:protect" or
+        node2.name == "protector:protect2" or
+        node2.name == "protector_mese:protect" or
+        node2.name == "protector_mese:brazier_bronze" or
+        node2.name == "protector_mese:brazier_gold" or
+        node2.name == "doors:door_steel_b_1" or
+        node2.name == "doors:door_steel_t_1" or
+        node2.name == "doors:door_steel_b_2" or
+        node2.name == "doors:door_steel_t_2" or
+        node2.name == "electricity:piston2_on" or
+        node2.name == "electricity:piston_pusher_sticky"
+        or
+        node3.name == "protector:protect" or
+        node3.name == "protector:protect2" or
+        node3.name == "protector_mese:protect" or
+        node3.name == "protector_mese:brazier_bronze" or
+        node3.name == "protector_mese:brazier_gold" or
+        node3.name == "doors:door_steel_b_1" or
+        node3.name == "doors:door_steel_t_1" or
+        node3.name == "doors:door_steel_b_2" or
+        node3.name == "doors:door_steel_t_2" or
+        node3.name == "electricity:piston2_on" or
+        node3.name == "electricity:piston_pusher_sticky"
+
+    then
+        minetest.swap_node(pos, node)
+        if node0.name == "electricity:piston_pusher_part" then
+            minetest.set_node(node0_pos, { name = "air" })
+        end
+        if node1.name == "electricity:piston_pusher_sticky" then
+            minetest.set_node(node1_pos, { name = "air" })
+        end
+        return false
+    end
+
+    local meta2 = minetest.get_meta(node2_pos):to_table()
+    minetest.set_node(node2_pos, { name = "air" })
+    minetest.set_node(node0_pos, node2)
+    minetest.get_meta(node0_pos):from_table(meta2)
+
+    local meta3 = minetest.get_meta(node3_pos):to_table()
+    minetest.set_node(node3_pos, { name = "air" })
+    minetest.set_node(node1_pos, node3)
+    minetest.get_meta(node1_pos):from_table(meta3)
+
+    minetest.swap_node(pos, node)
+
+    minetest.sound_play("piston_retract", {
+        pos = pos,
+        max_hear_distance = 20,
+        gain = 0.3,
+    })
+    minetest.check_for_falling(node1_pos)
+
+    return true
+end
+
+-- Boxes:
+local pt = 3/16 -- pusher thickness
+
+local piston_pusher_part_box = {
+	type = "fixed",
+	fixed = {
+		{-3/16, -3/16, -.5 + pt, 3/16, 3/16,  .5 + pt},
+	},
+}
+
+local piston2_definition_base = {
+    description = "Electricity piston x2", -- sticky, 1 node long, also works as activator.
+    drop = "electricity:piston2_off",
+    tiles = {
+		"mesecons_piston_top.png",
+		"mesecons_piston_bottom.png",
+		"mesecons_piston_left.png",
+		"mesecons_piston_right.png",
+		"mesecons_piston_back.png",
+		"mesecons_piston_pusher_front_sticky.png"
+	},
+    paramtype2 = "facedir",
+    is_ground_content = false,
+    on_timer = function(pos, elapsed)
+        electricity.piston2_on_timer(pos, elapsed)
+        return true
+    end,
+    on_construct = function(pos)
+        local h = minetest.hash_node_position(pos)
+        electricity.not_producers[h] = pos
+        electricity.set(pos, pos, 0)
+        minetest.get_node_timer(pos):start(0.5)
+    end,
+	after_place_node = function(pos, placer)
+		local meta = minetest.get_meta(pos)
+		meta:set_string("owner", placer:get_player_name() or "")
+	end,
+    after_destruct = function(pos)
+        local h = minetest.hash_node_position(pos)
+        electricity.not_producers[h] = nil
+        electricity.rdata[h] = nil
+    end,
+    electricity = {
+        rules = {
+            {x=0,y=1,z=0},
+            {x=-1,y=0,z=0},
+            {x=0,y=0,z=1},
+            {x=0,y=0,z=-1},
+            {x=0,y=-1,z=0},
+        },
+        name_on = "electricity:piston2_on",
+        name_off = "electricity:piston2_off",
+    },
+    groups = {electricity = 1, electricity_consumer= 1, cracky = 3},
+    sounds = default.node_sound_wood_defaults(),
+}
+
+local piston2_definition = table.copy(piston2_definition_base)
+minetest.register_node("electricity:piston2_off", piston2_definition)
+
+piston2_definition = table.copy(piston2_definition)
+piston2_definition.drawtype = "nodebox"
+piston2_definition.tiles = {
+    "mesecons_piston_top.png",
+    "mesecons_piston_bottom.png",
+    "mesecons_piston_left.png",
+    "mesecons_piston_right.png",
+    "mesecons_piston_back.png",
+    "mesecons_piston_on_front.png"
+}
+piston2_definition.node_box = piston_on_box
+piston2_definition.selection_box = piston_on_box
+piston2_definition.paramtype = "light"
+piston2_definition.groups["not_in_creative_inventory"] = 1
+piston2_definition.electricity.rules = {
+    {x=0,y=1,z=0},
+    {x=-1,y=0,z=0},
+    {x=0,y=0,z=1},
+    {x=0,y=0,z=-1},
+    {x=0,y=-1,z=0},
+}
+piston2_definition.on_rotate = function(pos, node, player, mode)
+    return false
+end
+piston2_definition.on_destruct = function(pos)
+    local face_vector = electricity.get_node_face_direction(pos)
+    local node0_pos = electricity.get_pos_relative(pos, {x=1,y=0,z=0}, face_vector, null)
+    local node0_name = minetest.get_node(node0_pos).name
+    local node1_pos = electricity.get_pos_relative(pos, {x=2,y=0,z=0}, face_vector, null)
+    local node1_name = minetest.get_node(node1_pos).name
+    -- make sure there actually is a pusher
+	if node0_name ~= "electricity:piston_pusher_part" then
+		return
+	end
+    minetest.remove_node(node0_pos)
+	if node1_name ~= "electricity:piston_pusher_sticky" then
+		return
+	end
+    minetest.remove_node(node1_pos)
+
+	minetest.sound_play("piston_retract", {
+		pos = pos,
+		max_hear_distance = 20,
+		gain = 0.3,
+	})
+end
+minetest.register_node("electricity:piston2_on", piston2_definition)
+
+-- pusher (part of piston)
+minetest.register_node("electricity:piston_pusher_part", {
+	description = "Sticky Piston Pusher extender",
+	drawtype = "nodebox",
+	tiles = {
+		"mesecons_piston_pusher_top.png",
+		"mesecons_piston_pusher_bottom.png",
+		"mesecons_piston_pusher_left.png",
+		"mesecons_piston_pusher_right.png",
+		"mesecons_piston_pusher_back.png",
+		"mesecons_piston_pusher_front_sticky.png"
+	},
+	groups = {not_in_creative_inventory = 1, cracky = 3},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	is_ground_content = false,
+	diggable = false,
+	selection_box = piston_pusher_part_box,
+	node_box = piston_pusher_part_box,
 	drop = "",
 	sounds = default.node_sound_wood_defaults(),
 })
